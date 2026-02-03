fa1269458c6acec86c4f32791e36d01de0d223f0
artemiy-code <artem.grg@mail.ru>
2026-02-03 07:01:41 +0300

    commit code beautify2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,6 @@ android {
         version = release(36)
     }
 
-
     defaultConfig {
         applicationId = "ru.artem_torpedo.diabetesdiary"
         minSdk = 26
@@ -55,20 +54,16 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    // ===== Room =====
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
 
-// ===== ViewModel & LiveData =====
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
 
-// ===== Coroutines =====
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
 
     implementation("com.google.code.gson:gson:2.11.0")
-
 
 }
